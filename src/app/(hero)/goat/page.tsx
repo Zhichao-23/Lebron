bdc5a04bb10edd5285917331099b29b79d6af8b7
柳志超 <lzc1548410248@qq.com>
2025-02-22 22:29:04 +0800

    fix:root layout export a metaData

--- src/app/(hero)/goat/page.tsx
+++ src/app/(hero)/goat/page.tsx
@@ -1,6 +1,11 @@
 import Hero from '@/components/hero';
 import React from 'react';
 import Lebron1 from "@/assets/Lebron1.jpg";
+import type { Metadata } from 'next';
+
+export const metadata: Metadata= {
+  title: "Goat",
+}
 
 export default function GoatPage() {
   // notFound();
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import "./globals.css";
-import { Metadata } from "next";
 import { Comforter } from "next/font/google";
-
-export const metaData: Metadata = {
-  title: "Home"
-};
 
 const ComforterFont = Comforter({
   weight: "400",
